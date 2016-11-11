5ac68888d0628e7bcd95cf82458067017c407c46
Glen Wilcox <gdwinco@gmail.com>
2016-11-11 09:25:30 -0700

    fix to scale scripts

--- scale.sh
+++ scale.sh
@@ -11,8 +11,9 @@ echo "	--> Find the application replication controller"
 OPENSHIFT_APPLICATION_REPLICATION_CONTROLLER=`oc get rc -l app=fuse-amq | sed '1d' | awk '$2 > 0 { printf $1 }'`
 ! [ $? == 0 ] && echo "FAILED" && exit 1
 echo "		--> Found ${OPENSHIFT_APPLICATION_REPLICATION_CONTROLLER}"
-echo "	--> Scaling application to 3 instances"
-oc scale rc/${OPENSHIFT_APPLICATION_REPLICATION_CONTROLLER} --replicas=3
+SCALE_TO=2
+echo "	--> Scaling application to $SCALE_TO instances"
+oc scale rc/${OPENSHIFT_APPLICATION_REPLICATION_CONTROLLER} --replicas=$SCALE_TO
 ! [ $? == 0 ] && echo "FAILED" && exit 1
 echo "	--> Forcefully destroy the original pod"
 oc delete pod ${OPENSHIFT_AMQ_BROKER_POD_ORIGINAL}
